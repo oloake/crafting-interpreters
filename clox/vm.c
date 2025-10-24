9a9f46892df9e142b865c939f6b641e0fa3497de
olof.akesson@verisure.com <olof.akesson@verisure.com>
2025-10-24 09:52:18 +0200

    Scanning on Demand

--- clox/vm.c
+++ clox/vm.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 #include <stdio.h>
@@ -89,8 +90,7 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char* source) {
+  compile(source);
+  return INTERPRET_OK;
 }
