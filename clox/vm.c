8747a5b81c3b3e93ac4490237da00c8dacf8a02f
olof.akesson@verisure.com <olof.akesson@verisure.com>
2025-10-31 11:14:12 +0100

    Hash tables

--- clox/vm.c
+++ clox/vm.c
@@ -30,10 +30,12 @@ static void runtimeError(const char* format, ...) {
 void initVM() {
   resetStack();
   vm.objects = NULL;
+  initTable(&vm.strings);
 }
 
 void freeVM() {
-   freeObjects();
+  freeTable(&vm.strings);
+  freeObjects();
 }
 
 void push(Value value) {
